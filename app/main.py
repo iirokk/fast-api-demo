257596a8fee87ef6294b1603807176cfb728b607
Iiro Kokkonen <iiro.kokkonen@gmail.com>
2024-01-02 20:12:33 +0200

    cleanup

--- app/main.py
+++ app/main.py
@@ -28,9 +28,9 @@ def list_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_items(db, skip=skip, limit=limit)
 
 
-@app.get("/items/{id}", response_model=schemas.Item)
-def get_item(id: int, db: Session = Depends(get_db)):
-    db_item = crud.get_item(db, id)
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def get_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id)
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
